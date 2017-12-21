8258ed1f34b48a9587686361b685177fe40d9fcf
yumi <yumeishih17@gmail.com>
2017-12-21 17:48:33 +0800

    clean the dic

--- Q1.py
+++
@@ -1,6 +0,0 @@
-def ReverseString (inputString):
-    outputString = inputString[::-1]
-    return outputString
-
-inputString = input("Please input a string: ")
-print(ReverseString(inputString))
--- Q2.py
+++
@@ -1,9 +0,0 @@
-def IsPerfectSquare(number):
-    factor = 1
-    while(number>0):
-        number = number - factor
-        factor = factor+2;
-    return 0==number
-
-number = int(input("please input a number: "))
-print(IsPerfectSquare(number))
