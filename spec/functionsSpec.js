01e88775c1b6e79576669b6897efd45a42bacfa4
yumi <yumeishih17@gmail.com>
2018-01-02 17:51:05 +0800

    Add test: _isValidate success case

--- spec/functionsSpec.js
+++ spec/functionsSpec.js
@@ -1,7 +1,7 @@
-var Functions = require('../src/functions.js');
+var Table = require('../src/functions.js');
 var Function = new Table();
 
-describe('_IsValidFormat',function(){
+describe('_isValidFormat',function(){
     var fakeForm;
     beforeEach(function(){
         fakeForm = {
@@ -12,35 +12,50 @@ describe('_IsValidFormat',function(){
         spyOn(Function,"alertMsg");
     });
     it('success',function(){
-        expect(Function._IsValidFormat(fakeForm)).toBeTruthy();
+        expect(Function._isValidFormat(fakeForm)).toBeTruthy();
     });
     it('name format error',function(){
         fakeForm.name = "      "; 
-        expect(Function._IsValidFormat(fakeForm)).toBeFalsy();
+        expect(Function._isValidFormat(fakeForm)).toBeFalsy();
     });
     it('phone format error',function(){
         fakeForm.phone = "aaaaaaa";  
-        expect(Function._IsValidFormat(fakeForm)).toBeFalsy();
+        expect(Function._isValidFormat(fakeForm)).toBeFalsy();
     });
     it('email format error',function(){
         fakeForm.email = "wrongEmailFomat"; 
-        expect(Function._IsValidFormat(fakeForm)).toBeFalsy();
+        expect(Function._isValidFormat(fakeForm)).toBeFalsy();
     });
 });
 
-describe('_IsValidate',function(){
+describe('_isValidate',function(){
     var fakeForm;
+    var fakeDatas;
     var index = 0;
     beforeEach(function(){
+        mockLocalStorage();
+        fakeDatas=[
+            {
+                name: "fifi",
+                phone: "0900000000",
+                email: "fifi@mail.com" 
+            },           
+            {
+                name: "yiyi",
+                phone: "0900000000",
+                email: "yiyi@mail.com" 
+            },
+        ];
         fakeForm = {
             name: "kiki",
             phone: "0900000000",
             email: "kiki@mail.com" 
         }
         spyOn(Function,"alertMsg");
+        spyOn(JSON, 'parse').and.returnValue(fakeDatas);
     });
     it('success',function(){
-        expect(Function._IsValidate(fakeForm,index)).toBeTruthy();
+        expect(Function._isValidate(fakeForm,index)).toBeTruthy();
     });
     
     
